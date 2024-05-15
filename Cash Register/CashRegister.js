2415b481dc1e2e3e502d32e713216546a46f688b
mattclister <125405381+mattclister@users.noreply.github.com>
2024-05-15 09:59:38 +0100

    Update CashRegister.js

--- Cash Register/CashRegister.js
+++ Cash Register/CashRegister.js
@@ -1,12 +1,17 @@
 function checkCashRegister(price, cash, cid) {
-  console.log("--------------NEW ENTRY--------------------")
-  console.log(`PRICE: ${price} CASH: ${cash}
-  CID: ${cid}`)
-  // Change due is calculated
-    let change = cash - price
+
+
+// Change due is calculated
+let change = cash - price
+
+console.log(
+"--------------------------------NEW ENTRY----------------------------------")
+console.log(`PRICE: ${price} CASH: ${cash} CHANGE: ${change}
+CID: ${cid}
+----------------------------------------------------------------------------`)
   
-  // Dictionary of coin values
-    let coinNameValues = {
+// Dictionary of coin values
+let coinNameValues = {
       "PENNY": 0.01,
       "NICKEL": 0.05,
       "DIME": 0.1,
@@ -16,51 +21,88 @@ function checkCashRegister(price, cash, cid) {
       "TEN": 10,
       "TWENTY": 20,
       "ONE HUNDRED": 100
-    }
-  
-  // Function creating an object for each "Drawer of coins" in the cash register
-    function drawer(name,amount) {
-      let value = coinNameValues[name]
-      this.name = name;
-      this.amount = amount;
-      this.value = value
-      this.units = Math.round(amount/value);
-    }
-  
-  // Create a drawer for each corresponding drawer in CID, add to drawers
-  let drawers = []
-  for (let i = 0; i < cid.length; i++) {
-     drawers.push(new drawer(cid[i][0],cid[i][1]))
+    };
+
+let cashOut = {}
+
+// Function to create drawers
+function CreateDrawer(coinName,amount) {
+  this.name = coinName;
+  this.value = coinNameValues[coinName];
+  this.amount = amount;
+  this.units = Math.round(this.amount / this.value);
+  this.active = true
+};
+
+// Function to remove a unit
+function CashOut(coinName) {
+  console.log(`start: COIN ${coinName} VALUE: ${cashdrawer[coinName]["value"]} CHANGE: ${change}`)
+  cashdrawer[coinName]["amount"] = Math.round(cashdrawer[coinName]["amount"]*100 - cashdrawer[coinName]["value"]*100)/100;
+  cashdrawer[coinName]["units"] -= 1;
+  change = Math.round(change*100 - cashdrawer[coinName]["value"]*100)/100
+  if (cashOut.hasOwnProperty(coinName)) {cashOut[coinName] = Math.round(cashOut[coinName]*100 + cashdrawer[coinName]["value"]*100)/100}
+  else {cashOut[coinName] = cashdrawer[coinName]["value"]}
+  console.log(`end: COIN ${coinName} VALUE: ${cashdrawer[coinName]["value"]} CHANGE: ${change}`)
+}
+
+// Function to check if coin active
+function CheckActive(coinName) {
+  if (cashdrawer[coinName]["value"] <= change & cashdrawer[coinName]["units"] >= 1) {
+    return true
+  } else {return false}
+}
+
+// Create drawers
+let cashdrawer = {};
+for (let i = 0 ; i < cid.length ; i++) {
+  let coinName = cid[i][0];
+  let coinAmount = cid[i][1];
+  cashdrawer[coinName] = (new CreateDrawer(coinName,coinAmount))
   }
-  
-  // Resolve what change to give with recursive function
-  let coinArray = {}
-  
-  function changeCalc(change) {
-    console.log(`-------------------
-  Change: ${change}`)
-    if (change <= 0) {return coinArray} else {
-      for (let i = drawers.length-1;i>=0;i--) {
-        if (change >= drawers[i]["value"] & drawers[i]["units"] > 1) {
-            console.log(`Change Taken: ${drawers[i]["value"]}`)
-            drawers[i]["units"] = drawers[i]["units"] - 1
-            drawers[i]["amount"] = drawers[i]["amount"] - drawers[i]["value"]
-            change = Math.round(change - drawers[i]["value"])
-            coinArray[drawers[i]["name"]] = drawers[i]["value"]])
-            console.log(drawers)
-            return changeCalc(change)
-        }
+
+// Calculate change
+
+for (let i = Object.keys(coinNameValues).length -1; i >= 0; i--){
+  let prop = Object.keys(coinNameValues)[i]
+  cashdrawer[prop]["active"] = CheckActive(prop)
+  console.log(`Checking coin: ${cashdrawer[prop]["name"]}, ${cashdrawer[prop]["active"]}`)
+    while (cashdrawer[prop]["active"] == true) {
+      CashOut(cashdrawer[prop]["name"])
+      cashdrawer[prop]["active"] = CheckActive(prop)
+    }
+  }
+
+// Calculate Total Amount
+
+let totalAmount = 0;
+for (let i = 0; i<Object.entries(cashdrawer).length;i++) {
+  totalAmount = totalAmount + Math.round(Object.entries(cashdrawer)[i][1]["amount"])
+}
+
+// Create Output
+
+      let output = {status: "OPEN", change: Object.entries(cashOut)}
+
+      // Check funds sufficient
+      if (change > 0) {
+        output.status = "INSUFFICIENT_FUNDS";
+        output.change = []
       }
-    }
-  }
-  changeCalc(change)
-  
-  // Summarise Coins
-  
-  console.log(coinArray)
-  
-  // Sum by coin
-  
-  }
+
+      // Check if till is empty
+      console.log(`Total amount = ${totalAmount}`)
+      console.log(`Change = ${change}`)
+
+      if (totalAmount == 0 & change == 0) {
+        output.status = "CLOSED";
+        output.change = cid
+      }
+
+      // Return Output
+
+      console.log(output)
+      return output
+
+}
   
   checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
